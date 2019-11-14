cbe369a93c5effbf55e51ff2e2d6f0cff30183bc
user2 <user2@KH_I>
2019-11-14 21:37:31 +0900

    2019-11-14 봉운- 수정1

--- podo/src/main/java/com/ch/podo/collection/model/vo/Collection.java
+++ podo/src/main/java/com/ch/podo/collection/model/vo/Collection.java
@@ -1,5 +1,28 @@
 package com.ch.podo.collection.model.vo;
 
-public class Collection {
+import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Collection {
+	
+	private int id;				// 컬렉션 아이디
+	private String title;		// 제목
+	private String content;		// 내용
+	private int likeCount;		// 좋아요 갯수
+	private int spoilerCount;	// 스포일러 갯수
+	private Date createDate;	// 최초작성일
+	private Date modifyDate;	// 최종수정일
+	private String status;		// 상태
+	private String open;		// 공개여부
+	
 }
