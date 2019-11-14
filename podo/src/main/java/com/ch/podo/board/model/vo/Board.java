1db2ab8094add54d7a23faca904ea938b114d4cd
humormagnate <humormagnate@gmail.com>
2019-11-14 18:35:33 +0900

    홈 추가

---
+++ podo/src/main/java/com/ch/podo/board/model/vo/Board.java
@@ -0,0 +1,5 @@
+package com.ch.podo.board.model.vo;
+
+public class Board {
+
+}
--- podo/src/main/java/com/ch/podo/detailFilm/model/vo/DetailFilm.java
+++ podo/src/main/java/com/ch/podo/detailFilm/model/vo/DetailFilm.java
@@ -1,5 +1,5 @@
 package com.ch.podo.detailFilm.model.vo;
 
-public class DetailFilmController {
+public class DetailFilm {
 
 }
