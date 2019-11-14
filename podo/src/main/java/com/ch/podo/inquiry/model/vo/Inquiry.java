96015251a9a30cd3dce8861887bdbba62d771929
smilech90 <smilech90@naver.com>
2019-11-14 21:29:41 +0900

    20191114 21:29 철환 이미지 문의 vo 추가

--- podo/src/main/java/com/ch/podo/inquiry/model/vo/Inquiry.java
+++ podo/src/main/java/com/ch/podo/inquiry/model/vo/Inquiry.java
@@ -1,5 +1,23 @@
 package com.ch.podo.inquiry.model.vo;
 
-public class Inquiry {
+import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Inquiry {
+	private int id;
+	private int type;
+	private String content;
+	private Date createDate;
+	private String answer;
+	private Date answerDate;
 }
