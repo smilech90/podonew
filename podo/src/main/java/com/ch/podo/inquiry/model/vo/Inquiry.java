7226ee618aba60969a0b14e63405a2a846141d9b
smilech90 <smilech90@naver.com>
2019-11-14 21:30:57 +0900

    2019

--- podo/src/main/java/com/ch/podo/inquiry/model/vo/Inquiry.java
+++ podo/src/main/java/com/ch/podo/inquiry/model/vo/Inquiry.java
@@ -14,10 +14,11 @@ import lombok.ToString;
 @Getter
 @ToString
 public class Inquiry {
-	private int id;
-	private int type;
-	private String content;
-	private Date createDate;
-	private String answer;
-	private Date answerDate;
+	private int id;			  // 문의번호
+	private int type;		  // 타입(신고 종류)
+	private String content;	  // 문의 내용
+	private String createDate;// 최초 작성일
+	private String status;	  // 상태
+	private String answer;	  // 문의 답변
+	private String answerDate;// 답변 작성일
 }
