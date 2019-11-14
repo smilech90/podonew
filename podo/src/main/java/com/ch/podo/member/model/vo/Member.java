4c2680ee771305a8d8d8edae64d30f1c631def76
gksgprud99 <gksgprud99@naver.com>
2019-11-14 21:54:07 +0900

    191114 혜경 member vo

--- podo/src/main/java/com/ch/podo/member/model/vo/Member.java
+++ podo/src/main/java/com/ch/podo/member/model/vo/Member.java
@@ -1,5 +1,34 @@
 package com.ch.podo.member.model.vo;
 
+import java.sql.Date;
+import java.util.ArrayList;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor 
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Member {
 
+	private int id;
+	private String image;
+	private String email;
+	private String pwd;
+	private String nickName;
+	private String status;
+	private Date enrollDate;
+	private Date modifyDate;
+	private String googleId;
+	private String kakaoId;
+	private String autho;
+	private ArrayList<Integer> genre;
+	
 }
