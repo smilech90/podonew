a329f7a9fca5413a68b1910c1a07824f985334ad
smilech90 <smilech90@naver.com>
2019-11-15 17:27:17 +0900

    dao service lmpl controller 추가

--- podo/src/main/java/com/ch/podo/ratingReview/controller/RatingReviewController.java
+++ podo/src/main/java/com/ch/podo/ratingReview/controller/RatingReviewController.java
@@ -1,5 +1,15 @@
 package com.ch.podo.ratingReview.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.ch.podo.ratingReview.model.service.RatingReviewService;
+
+@Controller
 public class RatingReviewController {
+	
+	
+	@Autowired
+	private RatingReviewService ratingReviewService;
 
 }
