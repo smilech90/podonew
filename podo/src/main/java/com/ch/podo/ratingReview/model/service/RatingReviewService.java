3cc893a14dc52f1fc2411e630997cae1d8e896b9
chulhwan <smilech90@naver.com>
2019-11-22 21:14:08 +0900

    2019 11 22 reivew랑 ratingreview합쳐서 review > dto로 통합

--- podo/src/main/java/com/ch/podo/ratingReview/model/service/RatingReviewService.java
+++ podo/src/main/java/com/ch/podo/ratingReview/model/service/RatingReviewService.java
@@ -1,19 +1,17 @@
 package com.ch.podo.ratingReview.model.service;
 
 import com.ch.podo.ratingReview.model.vo.RatingReview;
-import com.ch.podo.review.model.vo.Review;
+
 
 public interface RatingReviewService {
 	
-	// 영화 방사형그래프 상세리뷰 보기
-	RatingReview selectRatingReviewDetailView(int id);
-
-	// 리뷰 수정 조회용 서비스
-	RatingReview selectUpdateReview(int id);
-	
-	// 리뷰 6개 점수 수정용 서비스
-	int reviewUpdate(RatingReview rr);
-	
-
+	/*
+	 * // 영화 방사형그래프 상세리뷰 보기 RatingReview selectRatingReviewDetailView(int id);
+	 * 
+	 * // 리뷰 수정 조회용 서비스 RatingReview selectUpdateReview(int id);
+	 * 
+	 * // 리뷰 6개 점수 수정용 서비스 int reviewUpdate(RatingReview rr);
+	 * 
+	 */
 
 }
