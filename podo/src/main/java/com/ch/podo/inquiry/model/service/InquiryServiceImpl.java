feffb887dc8aef953edc85aa047a29852225f1d9
gksgprud99 <gksgprud99@naver.com>
2019-11-28 19:35:37 +0900

    191128 혜경_ 마이페이지 문의 조회 완료

--- podo/src/main/java/com/ch/podo/inquiry/model/service/InquiryServiceImpl.java
+++ podo/src/main/java/com/ch/podo/inquiry/model/service/InquiryServiceImpl.java
@@ -1,8 +1,11 @@
 package com.ch.podo.inquiry.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ch.podo.board.model.vo.PageInfo;
 import com.ch.podo.inquiry.model.dao.InquiryDao;
 import com.ch.podo.inquiry.model.vo.Inquiry;
 
@@ -15,5 +18,15 @@ public class InquiryServiceImpl implements InquiryService{
 	@Override
 	public int insertQuestion(Inquiry inq) {
 		return inquiryDao.insertQuestion(inq);
+	}
+
+	@Override
+	public int myPageInquiryListCount(String id) {
+		return inquiryDao.myPageInquiryListCount(id);
+	}
+
+	@Override
+	public  ArrayList<Inquiry> myPageSelectInquiryList(String id, PageInfo pi) {
+		return inquiryDao.myPageSelectInquiryList(id, pi);
 	}
 }
